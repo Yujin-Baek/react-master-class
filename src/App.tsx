0dcff2ea5be41a0b3568523207a330a5b260669b
rachel4w2 <rachel4w2@naver.com>
2024-03-18 11:21:43 +0900

    #3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <Circle borderColor="pink" bgColor="teal" />
-      <Circle text="im here" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={value}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
